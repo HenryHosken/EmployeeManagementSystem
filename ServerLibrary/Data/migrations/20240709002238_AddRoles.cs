5d03581d8a9e438fac331dc08dc9782e8736a330
henry hosken <henry_hosken12@outlook.com>
2024-07-08 21:23:23 -0300

    Erros resolvidos

---
+++ ServerLibrary/Data/migrations/20240709002238_AddRoles.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServerLibrary.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_departments_DepartmentId",
+                table: "Employees");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_departments",
+                table: "departments");
+
+            migrationBuilder.RenameTable(
+                name: "departments",
+                newName: "Departments");
+
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "ApplicationUsers",
+                newName: "FullName");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Departments",
+                table: "Departments",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "SystemRoles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SystemRoles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UseRoles",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RoleId = table.Column<int>(type: "int", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UseRoles", x => x.Id);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Departments_DepartmentId",
+                table: "Employees",
+                column: "DepartmentId",
+                principalTable: "Departments",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Departments_DepartmentId",
+                table: "Employees");
+
+            migrationBuilder.DropTable(
+                name: "SystemRoles");
+
+            migrationBuilder.DropTable(
+                name: "UseRoles");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Departments",
+                table: "Departments");
+
+            migrationBuilder.RenameTable(
+                name: "Departments",
+                newName: "departments");
+
+            migrationBuilder.RenameColumn(
+                name: "FullName",
+                table: "ApplicationUsers",
+                newName: "Name");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_departments",
+                table: "departments",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_departments_DepartmentId",
+                table: "Employees",
+                column: "DepartmentId",
+                principalTable: "departments",
+                principalColumn: "Id");
+        }
+    }
+}
