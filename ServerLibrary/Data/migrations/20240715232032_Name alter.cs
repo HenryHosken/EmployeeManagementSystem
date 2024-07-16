b447ff5bffb78222f0e20d7924690fe9d96210cc
henry hosken <henry_hosken12@outlook.com>
2024-07-16 20:45:31 -0300

    novas funções

---
+++ ServerLibrary/Data/migrations/20240715232032_Name alter.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ServerLibrary.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Namealter : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UseRoles",
+                table: "UseRoles");
+
+            migrationBuilder.RenameTable(
+                name: "UseRoles",
+                newName: "UserRoles");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UserRoles",
+                table: "UserRoles",
+                column: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_UserRoles",
+                table: "UserRoles");
+
+            migrationBuilder.RenameTable(
+                name: "UserRoles",
+                newName: "UseRoles");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_UseRoles",
+                table: "UseRoles",
+                column: "Id");
+        }
+    }
+}
