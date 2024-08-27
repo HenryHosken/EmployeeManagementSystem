b7bac1779f9a29b622a95a6584584a5465edc776
henry hosken <henry_hosken12@outlook.com>
2024-08-27 17:02:33 -0300

    1

--- Client/Program.cs
+++ Client/Program.cs
@@ -1,9 +1,9 @@
 using Blazored.LocalStorage;
 using Client;
-using Microsoft.AspNetCore.Components.Authorization;
 using ClientLibrary.Helpers;
 using ClientLibrary.Services.Contracts;
 using ClientLibrary.Services.Implementations;
+using Microsoft.AspNetCore.Components.Authorization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -19,10 +19,8 @@ builder.Services.AddHttpClient("SystemApiClient", client =>
 //builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri("http://localhost:5139") });
 builder.Services.AddAuthorizationCore();
 builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<GetHttpClient>();
 builder.Services.AddScoped<LocalStorageService>();
-builder.Services.AddScoped<CustomAuthenticationStateProvider, CustomAuthenticationStateProvider>();
+builder.Services.AddScoped<AuthenticationStateProvider, CustomAuthenticationStateProvider>();
 builder.Services.AddScoped<IUserAccountService, UserAccountService>();
-
 await builder.Build().RunAsync();
